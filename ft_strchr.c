c0aadb6b27a77337a9ffb166d67e1303431dd4d9
ngomes-t <ngomes-t@student.42sp.org.br>
2022-06-25 05:08:19 +0200

    finished strchr

---
+++ ft_strchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngomes-t <ngomes-t@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 21:22:41 by ngomes-t          #+#    #+#             */
+/*   Updated: 2022/06/21 19:41:06 by ngomes-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	if (!(ft_isascii(c)))
+		c = c % 256;
+	while (*str)
+	{
+		if (*str == c)
+			return ((char *)str);
+		str++;
+	}
+	if (*str == c)
+		return ((char *)str);
+	return (NULL);
+}
