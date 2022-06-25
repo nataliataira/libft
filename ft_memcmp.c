b91c21c1440682d96fae7c2b7adbb7cbd84e92c7
ngomes-t <ngomes-t@student.42sp.org.br>
2022-06-25 05:04:55 +0200

    finished memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngomes-t <ngomes-t@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 19:24:53 by ngomes-t          #+#    #+#             */
+/*   Updated: 2022/06/24 20:11:10 by ngomes-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*ss1;
+	unsigned char	*ss2;
+
+	i = 0;
+	ss1 = (unsigned char *)s1;
+	ss2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (ss1[i] == ss2[i] && ((n - 1) > i))
+		i++;
+	return (ss1[i] - ss2[i]);
+}
