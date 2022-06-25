78fde1c58bff2c18c6f5f9ac3232304d30985668
ngomes-t <ngomes-t@student.42sp.org.br>
2022-06-25 05:08:35 +0200

    finished strdup

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngomes-t <ngomes-t@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/15 19:27:58 by ngomes-t          #+#    #+#             */
+/*   Updated: 2022/06/23 06:09:42 by ngomes-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	dup = (char *)malloc(sizeof (char) * (i + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
