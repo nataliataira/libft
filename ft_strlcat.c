a26527e1ee2c6b3fcb2dc9711dafb1f4023de48a
ngomes-t <ngomes-t@student.42sp.org.br>
2022-06-25 05:10:52 +0200

    finished strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngomes-t <ngomes-t@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/15 02:19:07 by ngomes-t          #+#    #+#             */
+/*   Updated: 2022/06/15 03:39:51 by ngomes-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len_src;
+	size_t	len_dst;
+
+	i = 0;
+	len_src = ft_strlen(src);
+	len_dst = ft_strlen(dst);
+	if (size <= len_dst)
+		return (size + len_src);
+	else
+	{
+		while (src[i] && (size - 1) > (len_dst + i))
+		{
+			dst[len_dst + i] = src[i];
+			i++;
+		}
+		dst[len_dst + i] = '\0';
+	}
+	return (len_dst + len_src);
+}
